2ff436cd19a89904e2b52eee64073a7036c50194
Anuar <whitesunriseson@gmail.com>
2026-06-11 02:06:02 +0500

    Add If Else examples

---
+++ Practice2/if_else/if_elif_else.py
@@ -0,0 +1,8 @@
+a = 10
+
+if a < 5:
+    print("small")
+elif a < 15:
+    print("medium")
+else:
+    print("large")
---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,6 @@
+a = 3
+
+if a > 5:
+    print("a is greater than 5")
+else:
+    print("a is not greater than 5")
